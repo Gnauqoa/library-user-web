00668739fb016082455d0103656ada9875703f19
Gnauqoa <quanglng2022@gmail.com>
2023-06-04 21:19:08 +0700

    add appbar to test

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import theme from "./theme";
 import { ThemeProvider } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NormalLayout from "layouts/NormalLayout";
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route path="" component={<div></div>}></Route>
+          <Route path="" element={<NormalLayout />}></Route>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
