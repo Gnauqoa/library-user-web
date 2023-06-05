5c499f561e8a5d5f2f4b4034efa2f1f1ecf485f4
Gnauqoa <quanglng2022@gmail.com>
2023-06-05 13:43:56 +0700

    add normalLayout/Footer

--- src/layouts/NormalLayout/index.jsx
+++ src/layouts/NormalLayout/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AppBar from "components/AppBar";
 import { Outlet } from "react-router-dom";
+import Footer from "components/Footer";
 
 const NormalLayout = () => {
   return (
     <div className="flex flex-col">
       <AppBar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
